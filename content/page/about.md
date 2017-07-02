252a00917296e45f5b4b442e76a4870b75f2fa06
Evan Downing <epdowning@gmail.com>
2017-07-02 08:14:34 +0000

    Added about section. Added first post.

---
+++ content/page/about.md
@@ -0,0 +1,14 @@
+Title: About
+
+Evan is currently enrolled as a [Computer Science Ph.D.](http://www.cc.gatech.edu/phd-computer-science) student at
+[Georgia Institute of Technology](http://www.gatech.edu/) studying [Information Security](http://www.scs.gatech.edu/content/information-security).
+
+He works as a graduate student under [Dr. Wenke Lee](http://wenke.gtisc.gatech.edu/) in the [Institute for
+Information Security & Privacy (IISP)](http://iisp.gatech.edu/).
+
+For the full record of Evan's academic experience, please refer to his Curriculum
+Vitae above.
+
+After he graduates from graduate school, he is interested in continuing his
+research and teaching in an academic setting (e.g. a Laboratory Research
+Scientist and an Adjunct Professor at a neighboring University).
---
+++ pelicanconf.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+LOAD_CONTENT_CACHE = False
+
+AUTHOR = u'Evan Downing'
+SITENAME = u'Evan Downing'
+SITEURL = ''
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+PATH = 'content'
+PAGE_PATHS = ['page']
+ARTICLE_PATHS = ['blog']
+STATIC_PATHS = ['page','blog','image','file','favicon.ico']
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+
+ICONS = [
+    ('rss', 'feeds/all.rss.xml'),
+]
+
+MENUITEMS = (
+    ('About', '/pages/about.html'),
+    ('CV', '/file/Downing_CV.pdf'),
+    ('PGP', '/file/pgp_public.asc'),
+    ('Github', 'https://github.com/evandowning'),
+    ('Google Scholar', 'https://scholar.google.com/citations?user=SnJNwIAAAAAJ&hl=en'),
+)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+USE_FOLDER_AS_CATEGORY = True
+
+###############
+# Theme stuff #
+###############
+
+THEME = 'theme/pelican-blueidea'
+
+# Display pages list on the top menu
+DISPLAY_PAGES_ON_MENU = False
+
+# Display categories list on the top menu
+DISPLAY_CATEGORIES_ON_MENU = False
+
+# Display categories list as a submenu of the top menu
+DISPLAY_CATEGORIES_ON_SUBMENU = True
+
+# Display the category in the article's info
+DISPLAY_CATEGORIES_ON_POSTINFO = True
+
+# Display the author in the article's info
+DISPLAY_AUTHOR_ON_POSTINFO = False
+
+# Display the search form
+DISPLAY_SEARCH_FORM = False
+
+# Sort pages list by a given attribute
+#PAGES_SORT_ATTRIBUTE (Title)
+
+# Display the "Fork me on Github" banner
+#GITHUB_URL (None)
+
+# Blogroll
+#LINKS 
+
+# Social widget
+#SOCIAL

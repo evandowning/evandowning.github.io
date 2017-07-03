3aa44c0138efb8c7b41f3a122c4bb09abfac7ddb
Evan <epdowning@gmail.com>
2017-07-03 11:27:08 -0400

    Fixed CNAME issues and ATOM feed being posted.

--- publishconf.py
+++ publishconf.py
@@ -13,8 +13,11 @@ from pelicanconf import *
 SITEURL = ''
 RELATIVE_URLS = True
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
